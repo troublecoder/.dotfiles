ab1e0a8ee437c942843f29640154863459f4b94c
troublecoder <troublecoder@gmail.com>
2024-11-29 18:01:34 +0900

    PowerShell 모듈 추가

--- windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-oh-my-posh init pwsh | Invoke-Expression
+Import-Module -Name Microsoft.WinGet.CommandNotFound
 
-(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
-(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
+oh-my-posh init pwsh | Invoke-Expression
+uv generate-shell-completion powershell | Out-String | Invoke-Expression
+uvx --generate-shell-completion powershell | Out-String | Invoke-Expression
